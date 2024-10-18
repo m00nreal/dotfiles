f584216a58ed35d45c13553dc630dba9047b806d
moonreal <arel.jair@gmail.com>
2024-10-18 12:52:59 -0600

    jk keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<C-a>", "ggVG", { silent = true, desc = "Select all" })
+vim.keymap.set("i", "jk", "<ESC>", { silent = true, desc = "Convenient normal mode" })
